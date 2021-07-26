f0aca4fa9ae39b3ad9d90885b40ad19f0a16fa3c
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2021-07-26 18:13:12 -0400

    Init the notion

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+  </div> -->
+  <div class="top-row">
+    <div>
+      <img class="left-drizzy tl" src="/img/sad-drake.gif" alt="sad drake" />
+    </div>
+    <div>
+      <img class="tr" src="/img/sad-drake.gif" alt="sad drake" />
+    </div>
   </div>
   <router-view />
+  <div class="bottom-row">
+    <div>
+      <img class="left-drizzy bl" src="/img/sad-drake.gif" alt="sad drake" />
+    </div>
+    <div>
+      <img class="br" src="/img/sad-drake.gif" alt="sad drake" />
+    </div>
+  </div>
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  display: flex;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 }
 
 #nav {
@@ -26,5 +58,48 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.top-row,
+.bottom-row {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+:is(.top-row, .bottom-row) div {
+  padding: 3%;
+}
+
+.left-drizzy {
+  transform: scaleX(-1);
+}
+
+.tl,
+.bl {
+  animation: clockwise 5s linear infinite;
+}
+
+.tr,
+.br {
+  animation: counter-clockwise 5s linear infinite;
+}
+
+@keyframes clockwise {
+  from {
+    transform: scaleX(-1) rotate(360deg);
+  }
+  to {
+    transform: scaleX(-1) rotate(0deg);
+  }
+}
+
+@keyframes counter-clockwise {
+  from {
+    transform: rotate(360deg);
+  }
+  to {
+    transform: rotate(0deg);
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1 class="rainbow rainbow-text-animated">
+      I guess that's just the
+      <a
+        class="notion-link"
+        v-bind:href="notionLink"
+        target="_blank"
+        title="The notion"
+        >notion</a
+      >
+    </h1>
   </div>
 </template>
 
@@ -14,5 +22,55 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  notionLink =
+    "https://juniper-laundry-d0c.notion.site/The-notion-e3a037db2d20451085f48cd18027b461";
+}
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+}
+
+.notion-link {
+  text-decoration: none;
+  color: unset;
+}
+
+.rainbow {
+  text-align: center;
+  text-decoration: underline;
+  font-size: 32px;
+  letter-spacing: 3px;
+}
+.rainbow-text-animated {
+  background: linear-gradient(
+    to right,
+    #6666ff,
+    #0099ff,
+    #00ff00,
+    #ff3399,
+    #6666ff
+  );
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+  animation: rainbow_animation 6s ease-in-out infinite;
+  background-size: 400% 100%;
+}
+
+@keyframes rainbow_animation {
+  0%,
+  100% {
+    background-position: 0 0;
+  }
+
+  50% {
+    background-position: 100% 0;
+  }
+}
+</style>
